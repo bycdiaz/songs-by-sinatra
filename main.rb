c79647d302fcdf996b4ebc7d1c671a8d9e725d02
Carlos Diaz <cdiaz86@gmail.com>
2019-09-13 23:45:00 -0400

    Adds database configuration for production.

--- main.rb
+++ main.rb
@@ -3,6 +3,14 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require './song'
 
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
 configure do
   enable :sessions
   set :username, 'frank'
